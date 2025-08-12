3fe3af1fd0380e50b2210e4586e99ae75c45f68a
Huffmaar <54920264+Huffmaar@users.noreply.github.com>
2025-08-12 15:25:28 -0400

    Update cxg_etl_gen.py

--- OSEAN-CELLxGENE-DB/cxg_etl_gen.py
+++ OSEAN-CELLxGENE-DB/cxg_etl_gen.py
@@ -13,6 +13,7 @@ else:
 """
 # Implementation is only based on cellxgene.
 # This is very rough cataegory for ETL that is meant to just query experiments and results data.
+# Authors will have to define author metadata speciic entries in seperate version; this only provides a generic version that works with all CellxGene studies.
 
 import pandas as pd
 import numpy as np
@@ -20,24 +21,13 @@ import sys
 import anndata as ad
 
 
-if len(sys.argv) > 1:
+if len(sys.argv) == 3:
    filename = sys.argv[1]
+   studyname = sys.argv[2]
 else:
    print("Please provide the absolute path of the cellxgene a5hd file as a command-line argument.")
+   sys.exit()
 
-
-
-def dummy_loop(term, length):
-    #Must be string
-    try:
-        blanck = []
-        print(length)
-        for i in range(length):
-            blanck.append(length)
-            return blanck
-    except:
-        print("Invalid Inputs")
-    return 0
 
 def SEA_INIT():
     SEA_Study  = pd.DataFrame(columns=['study_id','study_type', 'study_focus', 'study_subject', 'study_type_id', 'study_name', 'study_description',
@@ -54,7 +44,7 @@ def SEA_INIT():
     SEA_Onto  = pd.DataFrame(columns=['ontology_id','documentation_id','ontology_name','ontology_string'])
     SEA_Orgo =  pd.DataFrame(columns=['organism_id','group_id','experiment_id','species','species_id','type', 'type_id',
                                       'age', 'age_unit','age_unit_id','sex','sex_id', 'reference_id' , 'reference_source' , 'comments'  ])
-    SEA_Occr  = pd.DataFrame(columns=['occurence_id','organism_id','occurence_name','occurence_id','occurence_severity','occurence_start_time','occurence_start_unit', 'occurence_start_id', 
+    SEA_Occr  = pd.DataFrame(columns=['occurence_id','organism_id','occurence_name','occurence_type_id','occurence_severity','occurence_start_time','occurence_start_unit', 'occurence_start_id', 
                                       'occurence_end_time','occurence_end_unit', 'occurence_end_id', 'reference_id', 'reference_source', 'comments' ])
     SEA_Samp  = pd.DataFrame(columns=['sample_id','group_id','organism_id', 'collection', 'collection_id', 'collection_time', 'collection_time_unit', 'collection_time_unit_id', 'T0_definition',
                                       'expsample_type','expsample_type_id', 'expsample_reference_id', 'expsample_reference_name',
@@ -95,9 +85,6 @@ def _source_keys(df1, df2, col1, col2):
 
     return outer_join_df
 
-from collections import Counter
-
-
 
 SEATemplates = SEA_INIT()
 cellxgene = filename
@@ -107,21 +94,23 @@ h5ad = read_cxg(cellxgene)
 vadata = h5ad[1]
 
 
-
-
 SEATemplates = SEA_INIT()
 
 STUDY = SEATemplates[0]
-STUDY['study_name'] = vadata['study_name'].unique()
 STUDY['study_type_id'] = 'OBI_0003697'
 STUDY['study_focus'] = vadata['disease'] # True only for this
 
 STUDY['reference_source'] = 'CellxGene'
+STUDY['study_name'] = vadata['study_name'].unique()
+
 STUDY['study_id'] = STUDY.index+1
 SEATemplates[0] = STUDY.set_index('study_id')
 
+DOCU = SEATemplates[1]
+DOCU['reference_id'] = studyname
+DOCU['reference_source'] = 'CellxGene'
+
 EXPER =  SEATemplates[2]
-#Placeholder for Experiments.
 EXPER['experiment_id'] = vadata['study_name'] + vadata['disease']
 EXPER['reference_source'] = 'CellxGene'
 EXPER = EXPER.drop_duplicates(subset='experiment_id')
@@ -147,6 +136,11 @@ ONTO = pd.concat([ONTO, ontology2.to_frame().T])
 ONTO = pd.concat([ONTO, ontology3.to_frame().T])
 SEATemplates[6] = ONTO.set_index('ontology_id')
 
+OCCUR = SEATemplates[8]
+OCCUR['organism_id'] = vadata['donor_id']
+OCCUR['occurence_type'] = vadata['disease']
+OCCUR['occurence_type_id'] = vadata['disease_ontology_term_id']
+SEATemplates[8] = ONTO.set_index('ontology_id')
 
 GROUP = SEATemplates[10]
 GROUP['group_id'] = vadata['disease'] + vadata['study_name'] + vadata['development_stage']
@@ -170,8 +164,6 @@ SEATemplates[7] = ORGO.drop_duplicates(subset='organism_id')
 INTER= SEATemplates[3]
 INTER['intervention_id'] = vadata['study_name']+vadata['donor_id']
 INTER['organism_id'] = vadata['donor_id']
-INTER['material'] = vadata['disease']
-INTER[INTER['material'] == 'influenza']['material_id'] == "NCBITaxon_11309"
 INTER['intervention_type'] = 'immune response'
 SEATemplates[3] = INTER.drop_duplicates(subset='intervention_id')
 
@@ -185,7 +177,6 @@ SAMP['group_id'] = vadata['disease'].values + vadata['study_name'].values + vada
 SAMP['biosample_type'] = vadata['tissue'].values
 SAMP['expsample_type'] =  vadata['cell_type'].values + ' ' + vadata['suspension'].values
 SAMP['sample_id'] = 'SAMP'+vadata.index
-#SAMP['reference_id']
 SEATemplates[9] = SAMP
 
 ASSAY=SEATemplates[4]
@@ -194,14 +185,12 @@ ASSAY["platform"]  = vadata['assay']
 
 SEATemplates[4]=ASSAY.drop_duplicates(subset='assay_id')
 
-# Generate Results, does not includ mapping of appropriate _ids
 RESULT = SEATemplates[5]
 RESULT['result_id'] =  'RSLT'+vadata.index
 RESULT['sample_id'] = 'SAMP'+vadata.index
 RESULT['experiment_id'] = vadata['study_name'].values + vadata['disease'].values
 RESULT['group_id'] = vadata['disease'].values + vadata['study_name'].values + vadata['development_stage'].values
 RESULT["assay_name"]  = vadata['study_name'].values + vadata['assay'].values + ' assay'
-#RESULT["data_dimensions"] = 22343
 RESULT['assay_type'] = 'single ' + vadata['suspension'].values +' RNA-seq'
 RESULT["datatype"] = "h5aD"
 RESULT["file_access"] = "CellxGene"
